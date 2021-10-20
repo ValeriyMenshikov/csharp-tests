345a40e300e1bba5912476103a477b226b166232
ValeriyMenshikov <team095hunter@gmail.com>
2021-10-20 23:15:47 +0300

    Добавил тест на создание контакта

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        public ContactData(string firstname,
+                           string middlename,
+                           string lastname = "",
+                           string nickname = "",
+                           string title = "",
+                           string company = "",
+                           string address = "",
+                           string home = "",
+                           string mobile = "",
+                           string work = "",
+                           string fax = "",
+                           string email = "",
+                           string email2 = "",
+                           string email3 = "",
+                           string homepage = "",
+                           string bday = "",
+                           string bmonth = "January",
+                           string byear = "",
+                           string aday = "",
+                           string amonth = "February",
+                           string ayear = "",
+                           string address2 = "",
+                           string phone2 = "",
+                           string notes = "")
+        {
+            Firstname = firstname;
+            Middlename = middlename;
+            Lastname = lastname;
+            Nickname = nickname;
+            Title = title;
+            Company = company;
+            Address = address;
+            Home = home;
+            Mobile = mobile;
+            Work = work;
+            Fax = fax;
+            Email = email;
+            Email2 = email2;
+            Email3 = email3;
+            Homepage = homepage;
+            Bday = bday;
+            Bmonth = bmonth;
+            Byear = byear;
+            Aday = aday;
+            Amonth = amonth;
+            Ayear = ayear;
+            Address2 = address2;
+            Phone2 = phone2;
+            Notes = notes;
+        }
+
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string Home { get; set; }
+        public string Mobile { get; set; }
+        public string Work { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string Homepage { get; set; }
+        public string Bday { get; set; }
+        public string Bmonth { get; set; }
+        public string Byear { get; set; }
+        public string Aday { get; set; }
+        public string Amonth { get; set; }
+        public string Ayear { get; set; }
+        public string Address2 { get; set; }
+        public string Phone2 { get; set; }
+        public string Notes { get; set; }
+    }
+}
